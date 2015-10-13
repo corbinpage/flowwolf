417992786dabee91ac3a5c40567f304084b9d951
Corbin Page <corbin.page@gmail.com>
2015-10-13 00:38:12 -0400

    Years left example working

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 var nools = require("node_modules/nools/index.js");
 
-var flow = nools.compile(__dirname + "/lifeExpectancy.nools");
+var flow = nools.compile(__dirname + "/lifeExpectancy.nools", {
+	name: "Life Expectancy",
+	define: {
+		Gender: Gender,
+		Country: Country,
+		Age: Age,
+		Result: Result
+	},
+	scope: {
+		logger: String,
+		lifeExpectancy: String
+	}
+});
 var firedRules = [];
 
 var Gender = flow.getDefined("gender");
 var Country = flow.getDefined("country");
+var Age = flow.getDefined("age");
 var Result = flow.getDefined("result");
 
-var session = flow.getSession(new Gender('M'), new Country('Andorra'));
+var session = flow.getSession(new Gender('M'), new Country('Andorra'), new Age(27));
 
 session
 .on("fire", function (ruleName) {
@@ -21,6 +34,7 @@ session
       } else{
         console.log("Rules fired: ", firedRules); //[ 'Hello World', 'Hello World2' ]
         console.log(session.getFacts());
+        // var result = session.getFacts(Result);
         console.log("done");
       }
-   })
+    })
