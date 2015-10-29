649a3f85c67eb40474ab33a4b36bddb138308108
Corbin Page <corbin.page@gmail.com>
2015-10-28 21:40:28 -0400

    Updated migration file

--- db/db.js
+++ db/db.js
@@ -1,4 +1,4 @@
-var Mongoskin = require('mongoskin');
-var db = Mongoskin.db("mongodb://localhost:27017/dev", {native_parser:true});
+var MongoClient = require('mongoskin');
+var db = MongoClient.db("mongodb://localhost:27017/dev", {native_parser: true});
 
 module.exports = db;
--- db/migrate.js
+++ db/migrate.js
@@ -3,15 +3,19 @@ var fs = require("fs");
 
 db.bind('flow');
 
-var noolsFilePath = __dirname + '/../models/lifeExpectancy.nools';
-var rules = fs.readFileSync(noolsFilePath, 'utf8');
+// var noolsFilePath = __dirname + '/../models/lifeExpectancy.nools';
+// var rules = fs.readFileSync(noolsFilePath, 'utf8');
 
 db.flow.insert({
 	title: "Life Expectancy",
-	slug: "life-expectancy",
-	inputs: ["Gender", "Country", "Age"],
-	outputs: ["YearsLeft", "LifeExpectancy"],
-	rules: rules
+	slug: "life-expectancy"
+	// inputs: ["Gender", "Country", "Age"],
+	// outputs: ["YearsLeft", "LifeExpectancy"]
+	// rules: rules
 });
 
-db.close();
+db.flow.find().toArray(function(err, items) {
+	console.log(items);
+	console.log(items.length);
+	db.close();
+});
